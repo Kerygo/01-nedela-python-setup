93d71b1864afd973971a032540e405a184e903f0
Gunta <gunta16@gmail.com>
2026-03-16 21:11:57 +0200

    koda maiņa

--- 02-NEDELA-PYTHON/eligibility.py
+++ 02-NEDELA-PYTHON/eligibility.py
@@ -1,14 +1,23 @@
-# eligibility.py - Atbilstības pārbaudītājs
+ZALS = '\033[92m'
+SARKANS = '\033[91m'
+RESET = '\033[0m'
 
-vecums = int(input("Ievadi savu vecumu: "))
-ir_students = input("Vai esi students? (j/n): ").lower() == 'j'
-
-# Kompleksā loģika: Pārbaudām vecumu un statusu
-if vecums >= 65:
-    print("Tu kvalificējies senioru atlaidei!")
-elif vecums >= 18 and vecums <= 26 and ir_students:
-    print("Tu kvalificējies studentu atlaidei!")
-elif vecums >= 18:
-    print("Tu esi pilngadīgs, bet atlaides nav.")
-else:
-    print("Tu esi nepilngadīgs.")
+while True:
+    print("\n--- ATLAIDES PĀRBAUDE ---")
+    ievade = input("Ievadi vecumu (vai 'q' lai izietu): ").lower()
+    
+    if ievade == 'q':
+        break
+        
+    try:
+        vecums = int(ievade)
+        is_student = input("Vai esi students? (j/n): ").lower() == 'j'
+        
+        # Galvenais nosacījums:
+        if (16 <= vecums <= 26) or is_student:
+            print(f"{ZALS}Apsveicam! Tev pienākas studenta atlaide.{RESET}")
+        else:
+            print(f"{SARKANS}Diemžēl atlaide nepienākas.{RESET}")
+            
+    except ValueError:
+        print("Lūdzu ievadi skaitli!")
